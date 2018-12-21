e74e6dece11ed4543618c5a81a605d2e2ce33b2b
thomasmalcherek <thomas.malcherek@cloud37.ch>
2018-12-21 16:13:48 +0100

    last update

---
+++ ArrayMapping/.forms/bom/com/example/arraymapping/ArraymappingPackage.js
@@ -0,0 +1,25 @@
+/**
+ * DO NOT EDIT: This is an automatically generated file.
+ * This provides an implementation of the package class com.example.arraymapping.ArraymappingPackage.
+ */
+
+com.tibco.data.Loader.classDefiner["com.example.arraymapping.ArraymappingPackage"] = function() {
+
+/**
+ * Constructor.
+ */
+var theClass = function() {
+};
+
+com.tibco.data.Loader.currentLoader.registerClass(theClass, "com.example.arraymapping.ArraymappingPackage");
+
+
+// Load the classes defined by this package.
+com.tibco.data.Loader.currentLoader.load("com.example.arraymapping.GS");
+
+// Load the associated factory.
+com.tibco.data.Loader.currentLoader.load("com.example.arraymapping.ArraymappingFactory");
+
+};
+
+com.tibco.data.Loader.classDefiner["com.example.arraymapping.ArraymappingPackage"]();
